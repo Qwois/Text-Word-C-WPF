bad08f4301837cb8d8d34c52ba18ba034894c2bc
0x1a01x <69690887+Qwois@users.noreply.github.com>
2024-01-19 11:31:43 +0400

    Add files via upload

---
+++ TextWorder/TextWorder/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TextWorder
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            CommandBindings.Add(new CommandBinding(ApplicationCommands.New, NewFile));
+            CommandBindings.Add(new CommandBinding(ApplicationCommands.Open, OpenFile));
+            CommandBindings.Add(new CommandBinding(ApplicationCommands.Save, SaveFile));
+            CommandBindings.Add(new CommandBinding(ApplicationCommands.Close, CloseFile));
+        }
+
+        private void NewFile(object sender, RoutedEventArgs e)
+        {
+            richTextBox.Document.Blocks.Clear();
+        }
+
+        private void OpenFile(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+            {
+                using (StreamReader streamReader = new StreamReader(openFileDialog.FileName))
+                {
+                    richTextBox.Document.Blocks.Clear();
+                    richTextBox.Document.Blocks.Add(new Paragraph(new Run(streamReader.ReadToEnd())));
+                }
+            }
+        }
+
+        public void MakeTextUnderlined(object sender, RoutedEventArgs e)
+        {
+            TextRange textRange = new TextRange(richTextBox.Selection.Start, richTextBox.Selection.End);
+            textRange.ApplyPropertyValue(Inline.TextDecorationsProperty, TextDecorations.Underline);
+        }
+
+        public void FormatAllText()
+        {
+            TextRange textRange = new TextRange(richTextBox.Document.ContentStart, richTextBox.Document.ContentEnd);
+            textRange.ApplyPropertyValue(Inline.TextDecorationsProperty, TextDecorations.Underline);
+        }
+
+        private void SaveFile(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                using (StreamWriter streamWriter = new StreamWriter(saveFileDialog.FileName))
+                {
+                    streamWriter.Write(new TextRange(richTextBox.Document.ContentStart, richTextBox.Document.ContentEnd).Text);
+                }
+            }
+        }
+
+        private void CloseFile(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
